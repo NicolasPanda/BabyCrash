5de6a018d7dbbb3756e2f276038afd16dd85b20a
NicolasPanda <91343504+NicolasPanda@users.noreply.github.com>
2021-09-30 10:00:23 -0400

    Cooldown

--- Assets/Entity/Baby/Player.cs
+++ Assets/Entity/Baby/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 {
     private static Rigidbody2D rb;
 
+    public Image imageCooldown;
+
     public static bool dead = false;
     public float playerSpeed = 3;
     public static float speed;
@@ -16,6 +18,10 @@ public class Player : MonoBehaviour
     public float downBoost = -10;
     public float resetTime = 0.3f;
     private float nullSpeed = 0;
+
+    private bool isCooldown = false;
+    [SerializeField] private float cooldownTime = 1.5f;
+    private float cooldownTimer;
 
     public Text inputCount;
 
@@ -37,22 +43,26 @@ public class Player : MonoBehaviour
         rb.velocity = new Vector2(speed, 0);
 
         rb.gravityScale = downSpeed;
+
+        imageCooldown.fillAmount = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!dead)
+        if (!dead && !isCooldown)
         {
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 rb.AddRelativeForce(new Vector2(0, upBoost), ForceMode2D.Impulse);
                 Invoke("ResetVelocity", resetTime);
+                useAbility();
             }
             if (Input.GetKeyDown(KeyCode.DownArrow))
             {
                 rb.AddRelativeForce(new Vector2(0, downBoost), ForceMode2D.Impulse);
                 Invoke("ResetVelocity", resetTime);
+                useAbility();
             }
         }
         if (balloonCount <= 0)
@@ -70,6 +80,30 @@ public class Player : MonoBehaviour
         if (balloonCount == 3)
         {
             rb.gravityScale = upSpeed;
+        }
+
+        if (isCooldown)
+        {
+            ApplyCooldown();
+        }
+    }
+
+    void useAbility()
+    {
+        isCooldown = true;
+        cooldownTimer = cooldownTime;
+    }
+    void ApplyCooldown()
+    {
+        cooldownTimer -= Time.deltaTime;
+        if (cooldownTimer < 0.0f)
+        {
+            isCooldown = false;
+            imageCooldown.fillAmount = 0.0f;
+        }
+        else
+        {
+            imageCooldown.fillAmount = cooldownTimer / cooldownTime;
         }
     }
 
